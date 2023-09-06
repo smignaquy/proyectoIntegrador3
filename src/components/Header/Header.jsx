66981e3559d268a9ca607d793795f9ad1e3755c9
BautyRemondino2 <bremondino@udesa.edu.ar>
2023-09-06 11:35:25 -0300

    chiches

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,17 +10,21 @@ class Header extends Component{
     render(){
         return(
             <nav>
-                <a href="index.html" class="title"><h1>Renée Music</h1></a>
-                <div>
-                    <form action="./detalles-busqueda.html" method="get">
-                    <input name="buscarr" type="search" placeholder="Buscar" class="buscar"/>
-                    <button type="submit" id="btn" class="btn"><i class="fa-solid fa-magnifying-glass"></i></button></form>
+                <div class='cabecera'>
+                    <a href="index.html"><h1>Renée Music</h1></a>
+                    <a href="index.html"><img src="./img/logo.jpg" alt="" class="logo"/></a>
+                    <div class='div_buscar'>
+                        <form action="./detalles-busqueda.html" method="get">
+                        <input name="buscarr" type="search" placeholder="Buscar" class="buscar"/>
+                        <button type="submit" id="btn" class="btn"><i class="fa-solid fa-magnifying-glass"></i></button></form>
+                    </div>
                 </div>
                 <div class="seccionesdiv">
                     <a href="./index.html" class="secciones">Home</a>
                     <a href="./playlist.html" class="secciones" >Playlist</a>
                     <a href="./generos.html" class="secciones">Generos</a>
                 </div>
+                <img src='./img/guitarrabanner.jpg' alt="" class="banner"/>
             </nav>
         )
     };
