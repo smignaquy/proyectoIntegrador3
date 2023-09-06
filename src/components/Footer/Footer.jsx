637ef18e5db7a76b659fbc6c8b072f1cec2db6d3
smignaquy <smignaquy@ydesa.edu.ar>
2023-09-06 09:37:16 -0300

    css y screens

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function Footer(){
-    return(
-        <div>
-            <div class="footer">
-                <a href="index.html"><p class="nombre">Renée Music</p></a>
-                <a href="instagram.com"><i class="fa-brands fa-instagram"></i></a>
-                <a href="twitter.com"><i class="fa-brands fa-twitter"></i></a>
-                <a href="facebook.com"><i class="fa-brands fa-facebook-square"></i></a>
+class Footer extends Component{
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div>
+                <div class="footer">
+                    <a href="index.html"><p class="nombre">Renée Music</p></a>
+                    <a href="instagram.com"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="twitter.com"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="facebook.com"><i class="fa-brands fa-facebook-square"></i></a>
+                </div>
+                <div class="integrantes">
+                    <p><strong>Integrantes : Remondino Bautista - Santiago Mignaquy - Malena Laborda Moreno</strong></p>
+                </div>
             </div>
-            <div class="integrantes">
-                <p><strong>Integrantes : Remondino Bautista - Santiago Mignaquy - Malena Laborda Moreno</strong></p>
-            </div>
-        </div>
-    );
+        )
+    };
 }
 
 export default Footer;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Header(){
-    return(
-        //header html
-        <nav>
-            <a href="index.html" class="title"><h1>Renée Music</h1></a>
-            <div>
-                <form action="./detalles-busqueda.html" method="get">
-                <input name="buscarr" type="search" placeholder="Buscar" class="buscar"/>
-                <button type="submit" id="btn" class="btn"><i class="fa-solid fa-magnifying-glass"></i></button></form>
-            </div>
-            <div class="seccionesdiv">
-                <a href="./index.html" class="secciones">Home</a>
-                <a href="./playlist.html" class="secciones" >Playlist</a>
-                <a href="./generos.html" class="secciones">Generos</a>
-            </div>
-        </nav>
-    );
+class Header extends Component{
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <nav>
+                <a href="index.html" class="title"><h1>Renée Music</h1></a>
+                <div>
+                    <form action="./detalles-busqueda.html" method="get">
+                    <input name="buscarr" type="search" placeholder="Buscar" class="buscar"/>
+                    <button type="submit" id="btn" class="btn"><i class="fa-solid fa-magnifying-glass"></i></button></form>
+                </div>
+                <div class="seccionesdiv">
+                    <a href="./index.html" class="secciones">Home</a>
+                    <a href="./playlist.html" class="secciones" >Playlist</a>
+                    <a href="./generos.html" class="secciones">Generos</a>
+                </div>
+            </nav>
+        )
+    };
 }
+
 export default Header;
