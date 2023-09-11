972c42d2c2ebd03b07a045c2b8d0b8032650befb
smignaquy <smignaquy@ydesa.edu.ar>
2023-09-11 10:49:20 -0300

    ruteo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Index from './components/Index/Index'
+import Detalle from './screens/Detalle/Detalle'
+import NotFound from './screens/NotFound/NotFound'
 
 function App() {
   return (
-    <nav>
+    <>
       <Header/>
-      <Index/>
+      <Switch>
+        <Route path='/' exact={true} component={Index} />
+        <Route path='/detalle/artista/:id' component={Detalle} />
+        <Route path='/detalle/album/:id' component={Detalle}/>
+        <Route path='/detalle/cancion/:id' component={Detalle}/>
+        <Route path='' component={NotFound}/>
+      </Switch>
       <Footer/>
-    </nav>
-  
+    </>
   );
 }
 
--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 class Index extends Component{
     constructor(){
@@ -33,21 +34,25 @@ class Index extends Component{
         return(     
             <section>
                 <h2 className="h2artistas">TOP Artistas</h2>
-                <main className="cantantes">
-                    {this.state.dataArtist.length === 0 ? (
-                        <img src='./img/loadingGif.gif' alt='Espere a que carge..' className="gif"/>
-                    ) : (
-                        this.state.dataArtist.slice(5).map((unArtista,idx ) => (
-                            <div key={idx} className='album'>
-                                <img src={unArtista.picture} alt="" />
-                                <p key={idx}>{unArtista.name}</p>
-                            </div>
-                        ))
-                    )
-                    
-                }
+                    <main className="cantantes">
+                        {this.state.dataArtist.length === 0 ? (
+                            <img src='./img/loadingGif.gif' alt='Espere a que carge..' className="gif"/>
+                        ) : (
+                            this.state.dataArtist.slice(5).map((unArtista,idx ) => (
+                                <div key={idx} className='album'>
+                                    <Link to={`/detalle/artista/${unArtista.id}`}>
+                                        <img src={unArtista.picture} alt="" />
+                                        <p className="nombreHome" key={idx}>{unArtista.name}</p>
+                                    </Link>
+                                </div>
+                            ))
+                        )
+                        
+                    }
+                    </main>
 
-                </main>
+
+
                 <h2 className="h2albumes">TOP Albumes</h2>
                 <main className="albumesfotos">
                 {this.state.dataAlbums.length === 0 ? (
@@ -55,10 +60,12 @@ class Index extends Component{
                     ) : (
                         this.state.dataAlbums.slice(5).map((unAlbum, idx) => (
                             <div key={idx} className='album'>
-                              <img src={unAlbum.cover} alt="" />
-                              <div>
-                                <p>{unAlbum.title}</p>
-                              </div>
+                                <Link to={`/detalle/album/${unAlbum.id}`}>
+                                    <img src={unAlbum.cover} alt="" />
+                                    <div>
+                                        <p className="nombreHome" >{unAlbum.title}</p>
+                                    </div>
+                                </Link>
                             </div>
                           ))
                           
@@ -66,6 +73,8 @@ class Index extends Component{
                     
                 }
                 </main>
+
+
                 <h2 className="h2canc">TOP Canciones</h2>
                 <main className="cancionesindex">
                 {this.state.dataAlbums.length === 0 ? (
@@ -73,10 +82,12 @@ class Index extends Component{
                     ) : (
                         this.state.dataMusic.slice(5).map((unaMusica, idx) => (
                             <div key={idx} className='album'>
-                              <img src={unaMusica.artist.picture} alt="" />
-                              <div>
-                                <p>{unaMusica.title} de {unaMusica.artist.name}</p>
-                              </div>
+                                <Link to={`/detalle/cancion/${unaMusica.id}`}>
+                                    <img src={unaMusica.artist.picture} alt="" />
+                                    <div>
+                                        <p className="nombreHome">{unaMusica.title} de {unaMusica.artist.name}</p>
+                                    </div>
+                                </Link>
                             </div>
                           ))
                           
