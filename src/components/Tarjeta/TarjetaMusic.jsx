4b730a13cfea75ef081dfe0676dbff3d40b02a73
smignaquy <smignaquy@ydesa.edu.ar>
2023-09-14 22:21:15 -0300

    playlist

--- src/components/Tarjeta/TarjetaMusic.jsx
+++ src/components/Tarjeta/TarjetaMusic.jsx
@@ -6,12 +6,62 @@ class TarjetaMusic extends Component{
     constructor(props){
         super(props)
         this.state = {
+            unaMusica : this.props.data,
             verMas: false,
             textoVerMas: 'Ver Más',
-            miniDescripcion: {
-            },
+            miniDescripcion: {},
+            textoBoton: "Agregar a favoritos",
+            favoritos: []
         }
     }
+
+    componentDidMount(){
+        let arrayFavoritos = [];
+        let recuperoStorage = localStorage.getItem('favoritos');
+        
+        if(recuperoStorage !== null){
+            arrayFavoritos = JSON.parse(recuperoStorage);
+
+           if(arrayFavoritos.includes(this.state.unaMusica.id)){
+             this.setState({
+                 textoBoton: 'Quitar de favoritos'
+             })
+           }    
+        }
+    }
+
+    agregarAFavoritos(id){
+        // Agregar un id dentro de array y colocar ese array en localStorage
+        let arrayFavoritos = []
+        let recuperoStorage = localStorage.getItem('favoritos');
+        
+        if(recuperoStorage !== null){
+           arrayFavoritos = JSON.parse(recuperoStorage);   
+        }
+           
+        if(arrayFavoritos.includes(id)){
+            //Si el id está en el array queremos sacar el id.
+            arrayFavoritos = arrayFavoritos.filter( unId => unId !== id);
+
+            this.setState({
+                textoBoton: "Agregar a Favoritos"
+            })
+
+
+        } else {
+            arrayFavoritos.push(id);
+            this.setState({
+                textoBoton: "Quitar de favoritos"
+            })
+        }
+
+        //Subirlo a local storage stringifeado
+        let arrayFavoritosAString = JSON.stringify(arrayFavoritos)
+        localStorage.setItem('favoritos', arrayFavoritosAString)
+
+        console.log('storage', localStorage)
+    }
+
 
     botonVerMas(){
         if(this.state.verMas == false){
@@ -33,25 +83,26 @@ class TarjetaMusic extends Component{
     }
 
     render(){
-        console.log(this.props)
+        //console.log(this.props)
         // Accede a las propiedades de unArtista a través de this.props.data
         const unaMusica = this.props.data;
         return(
             <>
-            <div key={unaMusica.id} className='album'>
-                <Link to={`/detalle/artista/${unaMusica.id}`}>
-                    <img src={unaMusica.artist.picture} alt="" />
-                    <div className="nombresIndex">
-                        <p className="nombreHome" key={unaMusica.id}>{unaMusica.title}</p>
+                <div key={unaMusica.id} className='album'>
+                    <Link to={`/detalle/artista/${unaMusica.id}`}>
+                        <img src={unaMusica.artist.picture} alt="" />
+                        <div className="nombresIndex">
+                            <p className="nombreHome" key={unaMusica.id}>{unaMusica.title}</p>
+                        </div>
+                    </Link>
+                    <div className="width">
+                        <button id="btn" className="btnVer verIndex" onClick={() => this.botonVerMas()}>{this.state.textoVerMas}</button>
                     </div>
-                </Link>
-                <div className="width">
-                    <button id="btn" className="btnVer verIndex" onClick={() => this.botonVerMas()}>{this.state.textoVerMas}</button>
+                    <div className="miniDescrip">
+                        <p>{this.state.miniDescripcion.name}</p>
+                    </div>   
+                    <button id="btn" className="btnVer verIndex" onClick={()=>this.agregarAFavoritos(this.state.unaMusica.id)} type="button">{ this.state.textoBoton }</button>                            
                 </div>
-                <div className="miniDescrip">
-                    <p>{this.state.miniDescripcion.name}</p>
-                </div>                               
-            </div>
             </>
         )
     };
--- src/screens/Playlist/ScreenPlaylist.js
+++ src/screens/Playlist/ScreenPlaylist.js
@@ -2,13 +2,71 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import Favoritos from "../../components/Favoritos/Favoritos";
 import Playlist from '../../components/Playlist/Playlist'
+import TarjetaMusic from "../../components/Tarjeta/TarjetaMusic";
 
-function ScreenFavoritos() {
-    return (
-      <>
-        <Playlist />
-      </>
-    );
+class ScreenFavoritos extends Component{
+  constructor(){
+      super()
+      this.state = {
+        favoritos: [],
+      }
   }
+
+  componentDidMount(){
+    let arrayFavoritos = [];
+    let arrayFavs = [];
+    let recuperoStorage = localStorage.getItem('favoritos');
+      
+    if(recuperoStorage !== null){
+      arrayFavoritos = JSON.parse(recuperoStorage);
+      arrayFavoritos.map(function(id){
+        //llamo a la api
+        fetch('https://thingproxy.freeboard.io/fetch/https://api.deezer.com/track/'+id)
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          arrayFavs.push(data)
+          //console.log('array favs', arrayFavs)
+          this.setState({
+            favoritos : this.arrayFavs
+          })
+          console.log('array favs state', this.state.favoritos)
+        })
+        .catch(function(error){
+          console.log('El error es: ' + error);
+        })
+      })
+      //console.log('array favs', arrayFavs)
+    }
+    //console.log('array favs', arrayFavs)
+    // this.setState({
+    //   favoritos : arrayFavs
+    // })
+    //console.log('array favs state', this.state.favoritos)
+  }
+
+  render(){
+      return(
+          <>
+            <div className="playlist">
+              <h1> Esta es tu lista de favoritos: </h1>
+            </div>
+            <main className="cancionesindex">
+                {this.state.favoritos.length === 0 ? (
+                        <img src='./img/loadingGif.gif' alt='Espere a que carge..' className="gif"/>
+                    ) : (
+                        this.state.favoritos.map((unaMusica) => (
+                            <TarjetaMusic data={unaMusica} key={unaMusica.id}/>
+                          ))
+                          
+                    )
+                    
+                }
+                </main>
+          </>
+      )
+  };
+}
+
   
 export default ScreenFavoritos;
